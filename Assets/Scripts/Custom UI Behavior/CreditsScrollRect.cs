daf8647ae3d5a106eaabee8748ecbbd83354615e
Ignacio Mosconi <nacho7577@hotmail.com>
2019-03-07 12:13:36 -0300

    Fixed & Enhanced the Credits Screen

---
+++ Assets/Scripts/Custom UI Behavior/CreditsScrollRect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CreditsScrollRect : ScrollRect
+{
+    [SerializeField] float rollDuration;
+    float elapsedTime;
+    bool isRolling;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        verticalNormalizedPosition = 1f;
+        elapsedTime = 0f;
+        isRolling = true;
+    }
+
+    public override void OnBeginDrag(PointerEventData data)
+    {
+        base.OnBeginDrag(data);
+
+        isRolling = false;
+    }
+
+    public override void OnEndDrag(PointerEventData data)
+    {
+        base.OnEndDrag(data);
+
+        float interpolationValue = Mathf.InverseLerp(1f, 0f, verticalNormalizedPosition);
+
+        elapsedTime = rollDuration * interpolationValue;
+        isRolling = true;
+    }
+
+    void Update()
+    {
+        if (verticalNormalizedPosition > 0f && isRolling)
+        {
+            elapsedTime += Time.deltaTime;
+
+            verticalNormalizedPosition = Mathf.Lerp(1f, 0f, elapsedTime / rollDuration);
+        }
+    }
+}
