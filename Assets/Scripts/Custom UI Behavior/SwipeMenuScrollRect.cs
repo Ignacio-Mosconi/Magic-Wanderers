e71c5e42f5c627ae64747ac89be0156ac208ccd8
Ignacio Mosconi <nacho7577@hotmail.com>
2019-03-08 12:16:38 -0300

    Made a "Swipe-Like" Symbols Menu for the Life Panel

---
+++ Assets/Scripts/Custom UI Behavior/SwipeMenuScrollRect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwipeMenuScrollRect : ScrollRect
+{
+    [SerializeField] int numberOfSteps;
+    [SerializeField] float clampDuration;
+    int currentStep;
+    float previousScrollSensitivity;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        horizontalNormalizedPosition = 0f;
+        currentStep = 1;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        float currentNormalizedPosition = Mathf.InverseLerp(1, numberOfSteps, currentStep);
+
+        if (currentStep < numberOfSteps && horizontalNormalizedPosition > currentNormalizedPosition + currentNormalizedPosition / 2f)
+            currentStep++;
+
+        if (currentStep > 1 && horizontalNormalizedPosition < currentNormalizedPosition - currentNormalizedPosition / 2f)
+            currentStep--;
+
+        previousScrollSensitivity = scrollSensitivity;
+        scrollSensitivity = 0f;
+
+        StartCoroutine(ClampToStep());
+    }
+
+    IEnumerator ClampToStep()
+    {
+        float elapsedTime = 0f;
+        float startValue = horizontalNormalizedPosition;
+        float endValue = Mathf.InverseLerp(1, numberOfSteps, currentStep);
+
+        while (elapsedTime <= clampDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            horizontalNormalizedPosition = Mathf.Lerp(startValue, endValue, elapsedTime / clampDuration);
+
+            yield return null;
+        }
+
+        horizontalNormalizedPosition = endValue;
+        scrollSensitivity = previousScrollSensitivity;
+    }
+}
