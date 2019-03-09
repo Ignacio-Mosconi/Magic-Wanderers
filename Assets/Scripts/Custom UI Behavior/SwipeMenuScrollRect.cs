c50d05ef787c637029db2a5106557c8218544a65
Ignacio Mosconi <nacho7577@hotmail.com>
2019-03-08 22:10:53 -0300

    Enhanced the Symbols "Swipe-Like" Panel

--- Assets/Scripts/Custom UI Behavior/SwipeMenuScrollRect.cs
+++ Assets/Scripts/Custom UI Behavior/SwipeMenuScrollRect.cs
@@ -7,6 +7,8 @@ public class SwipeMenuScrollRect : ScrollRect
 {
     [SerializeField] int numberOfSteps;
     [SerializeField] float clampDuration;
+    [SerializeField] Image[] pageMarkers;
+    [SerializeField] Sprite[] pageMarkerSprites;
     int currentStep;
     float swapNormalizedDistance;
     float previousScrollSensitivity;
@@ -16,7 +18,7 @@ public class SwipeMenuScrollRect : ScrollRect
         base.Awake();
 
         float contentWidth = content.GetComponent<RectTransform>().sizeDelta.x;
-        float swapDistance = contentWidth / (numberOfSteps * 2f);
+        float swapDistance = contentWidth / (numberOfSteps * 4f);
         
         swapNormalizedDistance = Mathf.InverseLerp(0f, contentWidth, swapDistance);
     }
@@ -27,27 +29,39 @@ public class SwipeMenuScrollRect : ScrollRect
 
         horizontalNormalizedPosition = 0f;
         currentStep = 1;
+
+        pageMarkers[0].sprite = pageMarkerSprites[0];
+
+        for (int i = 1; i < pageMarkers.GetLength(0); i++)
+            pageMarkers[i].sprite = pageMarkerSprites[1];
     }
 
     public override void OnEndDrag(PointerEventData eventData)
     {
         base.OnEndDrag(eventData);
 
+        bool shouldChangeStep = false;
         float currentNormalizedPosition = Mathf.InverseLerp(1, numberOfSteps, currentStep);
 
         if (currentStep < numberOfSteps && horizontalNormalizedPosition > currentNormalizedPosition + swapNormalizedDistance)
+        {
             currentStep++;
+            shouldChangeStep = true;
+        }
 
         if (currentStep > 1 && horizontalNormalizedPosition < currentNormalizedPosition - swapNormalizedDistance)
+        {
             currentStep--;
+            shouldChangeStep = true;
+        }
 
         previousScrollSensitivity = scrollSensitivity;
         scrollSensitivity = 0f;
 
-        StartCoroutine(ClampToStep());
+        StartCoroutine(ClampToStep(shouldChangeStep));
     }
 
-    IEnumerator ClampToStep()
+    IEnumerator ClampToStep(bool changeStep)
     {
         float elapsedTime = 0f;
         float startValue = horizontalNormalizedPosition;
@@ -63,5 +77,19 @@ public class SwipeMenuScrollRect : ScrollRect
 
         horizontalNormalizedPosition = endValue;
         scrollSensitivity = previousScrollSensitivity;
+
+        if (changeStep)
+            ChangePageMarker();
+    }
+
+    void ChangePageMarker()
+    {
+        for (int i = 0; i < pageMarkers.GetLength(0); i++)
+        {
+            if (i + 1 == currentStep)
+                pageMarkers[i].sprite = pageMarkerSprites[0];
+            else
+                pageMarkers[i].sprite = pageMarkerSprites[1];
+        }
     }
 }
